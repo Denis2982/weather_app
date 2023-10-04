b0a7d41764f75972d49509ac7c127d9c914612c7
Denis2982 <denisdenis2982@gmail.com>
2023-10-04 19:06:39 +0300

    add function API get weather

--- App.js
+++ App.js
@@ -1,29 +1,44 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Alert, StyleSheet, Text, View } from 'react-native';
-import * as Location from 'expo-location';
-import Loading from './Loading';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
+import Loading from "./Loading";
+import axios from "axios";
+import Weather from "./Weather";
+
+const API_KEY = "3a3b263487223dba791502461c12d8ea";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+  };
 
   getLocation = async () => {
     try {
-      const response = await Location.requestForegroundPermissionsAsync();
-      const location = await Location.getCurrentPositionAsync();
+      await Location.requestForegroundPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
     } catch (error) {
-      Alert.alert('Невозможно определить местоположение');
-    }    
-  }
+      Alert.alert("Невозможно определить местоположение");
+    }
+  };
 
   componentDidMount() {
     this.getLocation();
   }
 
-  render(){
+  render() {
+    const {isLoading} = this.state;
     return (
-      <Loading />
-    );
+      isLoading ? <Loading /> : <Weather />
+      );
   }
 }
-
-
